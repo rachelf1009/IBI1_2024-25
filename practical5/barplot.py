7afc7e0063bfb6533cc1a0c0ff5bc40f4d590e1b
rachelf1009 <qiufan.24@intl.zju.edu.cn>
2025-03-19 13:17:42 +0800

    031901

--- practical5/barplot.py
+++ practical5/barplot.py
@@ -1,11 +1,18 @@
+
+#create my own directory and input the data
 language = {"JavaScript":62.3, "HTML": 52.9, "Python": 51, "SQL":51, "TypeScript": 38.5}
+#begin to draw pictures
 import matplotlib.pyplot as plt
-N = 5
+#define x and y axis
 x = list(language.keys())
 y = list(language.values())
+#define the width of the bar chart
 width = 0.5
+#plot the bar chart
 p1 = plt.bar(x,y,color = "red")
+#label the name of x and y axis and the title
 plt.ylabel("Population")
 plt.title("language population") 
 plt.xlabel("language")
+
 plt.show()
