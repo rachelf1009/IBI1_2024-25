4fe2a350a61514c103c419efa8784a2fa5af162e
rachelf1009 <qiufan.24@intl.zju.edu.cn>
2025-05-28 10:30:21 +0800

    revise practical8

--- practical8/drug dosage calculator.py
+++ practical8/drug dosage calculator.py
@@ -1,10 +1,14 @@
-def paracetamaol_volume (weight,strength):
+def paracetamol_volume (weight,strength):
     if not 10<weight<100  :
         print('your weight is out of range')
+        return
+    if strength not in [120, 250]:
+        print('Invalid strength')
+        return
     else:
         i=15*weight/strength*5
     return i
 
 weight=int(input('Your weight(kg) is:(kg)'))
 strength=int(input('Your paracetamol strength(mg/5ml) is:(_mg/5ml)'))
-print('suggested volume is', paracetamaol_volume(weight,strength))
+print('suggested volume is', paracetamol_volume(weight,strength))
