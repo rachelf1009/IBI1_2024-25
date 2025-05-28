7a06316e9b6b19380a260ca1f6e08149ed066174
rachelf1009 <qiufan.24@intl.zju.edu.cn>
2025-05-28 11:50:40 +0800

    revise practical14

--- practical14/DOM&SAX API.py
+++ practical14/DOM&SAX API.py
@@ -6,22 +6,30 @@ DOMTree = xml.dom.minidom.parse("/Users/xuqiufan/Desktop/IBI/IBI1_2024-25/practi
 collection = DOMTree.documentElement
 terms=collection.getElementsByTagName('term')
 
-max_term={"molecular_function": {"name": "", "count":0},
-          "biological_process": {"name": "","count": 0},
-          "cellular_component": {"name":"","count": 0}}
+ontology_max = {"molecular_function": [],"biological_process": [],"cellular_component": []}
+max_count = {"molecular_function": 0,"biological_process": 0,"cellular_component": 0}
 
 for term in terms:
     namespace=term.getElementsByTagName("namespace")[0].firstChild.nodeValue
-
     name=term.getElementsByTagName("name")[0].firstChild.nodeValue
-
     is_a=term.getElementsByTagName("is_a")
     is_a_count=len(is_a)
 
-    if namespace in max_term:
-        if is_a_count> max_term[namespace]["count"]:
-            max_term[namespace]={'name': name, "count":is_a_count}
-print(max_term)
+    if namespace in ontology_max:
+        if is_a_count > max_count[namespace]:
+            ontology_max[namespace] = [(name, is_a_count)]
+            max_count[namespace] = is_a_count
+        elif is_a_count == max_count[namespace]:
+            ontology_max[namespace].append((name, is_a_count))
+
+print("Results from DOM parser:")
+for ns, terms in ontology_max.items():
+    print(f"Ontology: {ns}")
+    for name, is_a_count in terms:
+        print(f"  Name: {name}")
+        print(f"  Number of <is_a>: {is_a_count}")
+        print()
+
 end=datetime.now()
 time1=end-start
 print(time1)
@@ -36,10 +44,8 @@ class GOHandler (xml.sax.ContentHandler):
         self.namespace = ""
         self.is_a_count = 0
         
-        self.max_counts = {"molecular_function": {"name": "", "count":0},
-          "biological_process": {"name": "","count": 0},
-          "cellular_component": {"name":"","count": 0}}
-
+        self.max_counts = {"molecular_function": {"terms": [], "count": 0},"biological_process": {"terms": [], "count": 0},"cellular_component": {"terms": [], "count": 0}}
+        
     def startElement(self, tag,attributes):
         self.current_element = tag
         if tag == "term":
@@ -60,11 +66,15 @@ class GOHandler (xml.sax.ContentHandler):
         
             
     def endElement(self, tag):
-        if tag == "term":
-            if self.namespace in self.max_counts:
-                if self.is_a_count > self.max_counts[self.namespace]["count"]:
-                    self.max_counts[self.namespace] = {"name":self.name, "count":self.is_a_count}
-
+      if tag == "term":
+        if self.namespace in self.max_counts:
+            current_max = self.max_counts[self.namespace]["count"]
+            term = (self.name, self.is_a_count)
+        if self.is_a_count > current_max:
+            self.max_counts[self.namespace]["terms"] = [term]
+            self.max_counts[self.namespace]["count"] = self.is_a_count
+        elif self.is_a_count == current_max:
+            self.max_counts[self.namespace]["terms"].append(term)
 
 parser = xml.sax.make_parser()
 Handler = GOHandler()
@@ -72,7 +82,14 @@ parser.setContentHandler(Handler)
 parser.setFeature(xml.sax.handler.feature_namespaces, 0)
 parser.parse('/Users/xuqiufan/Desktop/IBI/IBI1_2024-25/practical14/go_obo.xml')
 
-print(Handler.max_counts)
+print("Results from SAX parser:")
+for ns, data in Handler.max_counts.items():
+    print(f"Ontology: {ns}")
+    for name, is_a_count in data["terms"]:
+        print(f"  Name: {name}")
+        print(f"  Number of <is_a>: {is_a_count}")
+        print()
+
 end=datetime.now()
 time2=end-start
 print(time2)
